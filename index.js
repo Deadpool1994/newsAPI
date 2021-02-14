cb201364dbf2bfec3aed213f1ba39a9ca99091dc
deadpool1994 <mhtrami@gmail.com>
2021-02-15 01:35:01 +0530

    Add loadsh and fetch arguments :sparkles:

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hello world');
+const _ = require('lodash');
+
+var myArgs = process.argv.slice(2);
+const TOPIC = _.get(myArgs,'0',"COVID");
+const REGION = _.get(myArgs,'1',"CANADA");
+const PERIOD = _.get(myArgs,'2',48);
+
