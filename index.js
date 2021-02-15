c89493d73e0a0f99674bdb93ac3d35efd5e7fd5f
deadpool1994 <mhtrami@gmail.com>
2021-02-15 23:33:01 +0530

    refactor data and store in DB

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const _ = require('lodash');
 const fetch = require('node-fetch');
+const moment = require('moment');
+var db_lib = require('./db.js');
+console.log("DB: ",db_lib);
 
+// Fetch aeguments for topic, region, period
 var myArgs = process.argv.slice(2);
-const TOPIC = _.get(myArgs,'0',"COVID");
-const REGION = _.get(myArgs,'1',"CANADA");
-const PERIOD = _.get(myArgs,'2',48);
+const TOPIC = _.get(myArgs,'0',"COVID");              //TOPIC
+const REGION = _.get(myArgs,'1',"CANADA");            //COUNTRY
+const PERIOD = _.get(myArgs,'2',48);                  //PERIOD in hour
 
 var parseString = require('xml2js').parseString;
 
@@ -14,8 +18,18 @@ let url = `https://news.google.com/rss/search?q=%20%22${TOPIC}%22%20when%3A${PER
 (async function(){
 
     var xml = await fetch(url).then(res => res.text())
-    parseString(xml, function (err, result) {
-        console.dir(_.get(result,'rss.channel.0.item',[]));
+    parseString(xml, async function (err, result) {
+        let articles = _.get(result,'rss.channel.0.item',[]); 
+        articles = articles.map((article) => {
+          return {
+            title: _.get(article,'title.0',null),
+            url: _.get(article,'link.0',null),
+            published_date: _.get(article,'pubDate.0') ? moment(_.get(article,'pubDate.0')).unix() : moment().unix(),
+            publisher_name: _.get(article,'source.0._',null)
+          }
+        });
+        //console.log(articles);
+        await db_lib.saveBulkData(articles);
     });
 
 })();
